979cd4e74a64f96ff3f3fa6a9c9d80c663878ae2
Wunhae Kim 김은혜 <66160243+kimwunhae@users.noreply.github.com>
2023-04-17 12:09:41 +0900

    Init

---
+++ .storybook/preview.js
@@ -0,0 +1,22 @@
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    backgrounds: {
+      default: 'default',
+      values: [
+        { name: 'default', value: 'gray' },
+        { name: 'light', value: 'white' },
+        { name: 'dark', value: 'black' },
+      ],
+    },
+  },
+};
+
+export default preview;
